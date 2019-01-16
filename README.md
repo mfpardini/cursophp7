e32dafb71824213487e0dd9044f3d212a393ec0a
mfpardini <matheusfpardini@gmail.com>
2019-01-16 20:50:12 -0200

    criado arquivo para upload

--- README.md
+++
@@ -1,5 +0,0 @@
-# cursophp7
-
-Just a folder to keep the files .php
-
-I'm learning, so I need this
---
+++ upload/exemplo01.php
@@ -0,0 +1,33 @@
+<form method="post" enctype="multipart/form-data">
+	
+	<input type="file" name="fileUpload">
+	<br/>
+	<input type="submit" name="enviar">
+
+</form>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+	$file = $_FILES["fileUpload"];
+
+	if ($file["error"]) {
+		throw new Exception("Erro: ".$file["error"]);
+	}
+
+	$dirUploads = "uploads";
+
+	if (!is_dir($dirUploads)){
+		mkdir($dirUploads);
+	}
+
+	if(move_uploaded_file($file["tmp_name"], $dirUploads.DIRECTORY_SEPARATOR.$file["name"])) {
+		echo "Upload realizado com sucesso!";
+	} else {
+		throw new Exception("Ocorreu uma falha durante o Upload.<br/>Tente novamente...");
+	}
+
+}
+
+?>
